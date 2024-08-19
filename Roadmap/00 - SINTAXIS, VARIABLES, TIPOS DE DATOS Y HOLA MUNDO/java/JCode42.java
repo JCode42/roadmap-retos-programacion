6a9f370e66e14e2d352fcda1b1269b3eb2375f32
José <138406356+JCode42@users.noreply.github.com>
2024-08-18 22:15:16 -0500

    Add files via upload
    Ejercicion #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/JCode42.java
@@ -0,0 +1,31 @@
+public class JCode42 {
+    public static void main(String[] args) {
+
+        //  https://www.java.com/es/
+
+        //  Este es un ejemplo de comentario de una linea
+
+        /*  Este es un ejemplo
+            de comentario de
+            varias lineas
+        */
+
+        // Ejemplo de Variable
+        String miVariable;
+
+        // Ejemplo de Variable Constante
+        final String variableFinal;
+
+        // Tipos de Datos Primitivos
+        int miEntero;
+        float miFloat;
+        double miDouble;
+        boolean miBoolean;
+        char a;
+        String miString;
+
+        // Imprimir Texto "Hola, Java!"
+        System.out.println("Hola, Java!");
+
+    }
+}
